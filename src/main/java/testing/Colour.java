b16b6223b2e5eb541f88a30f60d9d838eb230514
Li Chengyi <120109345@umail.ucc.ie>
2020-12-16 17:40:00 +0800

    testColorValueRange test fail

--- src/main/java/testing/Colour.java
+++ src/main/java/testing/Colour.java
@@ -3,7 +3,14 @@ public class Colour {
     public final static String DEFAULT_MODEL = "RGB";
     //Color model
     private String model;
-
     //Color value
     private int value;
+
+    public Colour(String model, int red, int green, int blue){
+        if(model == null || DEFAULT_MODEL.equals(model)){
+            value = 0xff000000 | ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | blue & 0xFF;
+        }else{
+            value = 0;
+        }
+    }
 }
--- src/main/test/java/com.junit.test/ColourTest.java
+++ src/main/test/java/com.junit.test/ColourTest.java
@@ -2,9 +2,41 @@ package com.junit.test;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import testing.Colour;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
+
+    public Colour colour;
+
+    @DisplayName("test color value range")
+    @Test
+    public void testColorValueRange() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,-1, 0, 0);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,256, 0, 0);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,255, -1, 0);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,255, 256, 0);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,255, 255, -1);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    colour = new Colour(Colour.DEFAULT_MODEL,255, 255, 256);
+                });
+    }
 
 }
