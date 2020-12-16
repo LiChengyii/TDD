f227a1d3842ab57c06561620632552e3bbabe495
Li Chengyi <120109345@umail.ucc.ie>
2020-12-16 17:31:10 +0800

    Create class

---
+++ src/main/java/testing/Colour.java
@@ -0,0 +1,9 @@
+package testing;
+public class Colour {
+    public final static String DEFAULT_MODEL = "RGB";
+    //Color model
+    private String model;
+
+    //Color value
+    private int value;
+}
